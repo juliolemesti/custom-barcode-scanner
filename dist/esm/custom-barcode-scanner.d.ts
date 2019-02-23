c612063316f6e96f78b79edc76ff8608329e424d
Julio Lemes <juliolemesti@gmail.com>
2019-02-23 10:45:07 -0300

    chore(ionic native): remove ionic native class

--- dist/esm/custom-barcode-scanner.d.ts
+++
@@ -1,7 +0,0 @@
-import { IonicNativePlugin } from '@ionic-native/core';
-export declare class CustomBarcodeScanner extends IonicNativePlugin {
-    scan(options: ScannerOptions): Promise<string>;
-}
-export interface ScannerOptions {
-    torchOn: boolean;
-}
--- www/CustomBarcodeScanner.js
+++ www/CustomBarcodeScanner.js
@@ -3,8 +3,13 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'CustomBarcodeScanner';
 
 var CustomBarcodeScanner = {
-  scan: function (options, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, PLUGIN_NAME, 'scan', [options]);
+  scan: function (options, resolve, reject) {
+    exec(resolve, reject, PLUGIN_NAME, 'scan', [options]);
+  },
+  scanPromise: function (options) {
+    return new Promise((resolve, reject) => {
+      exec(resolve, reject, PLUGIN_NAME, 'scan', [options]);
+    });
   }
 };
 
